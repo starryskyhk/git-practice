be1049da1d32fc482e3a42677d269fd2b780f4de
Starry_sky <33302148+starryskyhk@users.noreply.github.com>
2020-04-01 21:31:17 +0800

    Update ceshi.java

--- src/demo/ceshi.java
+++ src/demo/ceshi.java
@@ -7,7 +7,7 @@ package demo;
 public class ceshi {
     public static void main(String[] args) {
         System.out.println("测dddd试");
-        System.out.println("韩坤测试");
-       System.out.println("第二次ffffffccff测试");
+        System.out.println("韩坤2测试");
+
     }
 }
