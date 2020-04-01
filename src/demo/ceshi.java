8b41006e8153e70373661fbcbfc085018518768c
Starry_sky <1545731887@qq.com>
2020-04-01 11:52:45 +0800

    Initial commit

---
+++ src/demo/ceshi.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @author 韩坤
+ * @create 2020-04-01-10:52
+ */
+public class ceshi {
+    public static void main(String[] args) {
+        System.out.println("测dddd试");
+        System.out.println("第二次ffffffccff测试");
+    }
+}
---
+++ src/demo/ceshi2.java
@@ -0,0 +1,11 @@
+package demo;
+
+/**
+ * @author 韩坤
+ * @create 2020-04-01-11:13
+ */
+public class ceshi2 {
+    public static void main(String[] args) {
+
+    }
+}
